531605f338a78f3ab03a292c462b2e4b72efab11
Ramesh Appat <rameshappat@gmail.com>
2025-12-25 16:30:20 -0700

    feat: add WebSecurityConfig.java

---
+++ src/main/java/com/organization/config/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.organization.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+/**
+ * Security configuration for OAuth2/JWT.
+ */
+@EnableWebSecurity
+@EnableResourceServer
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+            .antMatchers("/api/rates/**").authenticated()
+            .and()
+            .oauth2ResourceServer()
+            .jwt();
+    }
+}
